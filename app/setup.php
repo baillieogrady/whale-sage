9b649847dddf790a23f9ef3170ea3169e66f9036
Baillie O'Grady <baillieogrady@Baillies-iMac.local>
2020-04-16 14:29:42 +0100

    Change config so WordPress theme GUI upload is possible

--- app/setup.php
+++ app/setup.php
@@ -31,7 +31,7 @@ add_action('wp_enqueue_scripts', function () {
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
-    }
+    }:
 }, 100);
 
 /**
